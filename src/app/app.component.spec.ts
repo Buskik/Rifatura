8c204e6b3980f5acec1f3808689a1c4f5cd7e50b
Buskik <rdrade121@gmail.com>
2024-08-19 14:23:06 -0300

    Add filters to numbers (Asc, desc, search, available)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,18 +14,10 @@ describe("AppComponent", () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have the 'rifatura' title`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app.title).toEqual("rifatura");
-	});
-
 	it("should render title", () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector("h1")?.textContent).toContain(
-			"Hello, rifatura",
-		);
+		expect(compiled.querySelector(".box")).toBeTruthy();
 	});
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { CommonModule, NgFor } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import data from "../data.json";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet, NgFor, CommonModule],
+	imports: [RouterOutlet],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.less",
 })
 export class AppComponent {
-	numbers = data.numbers.map((e) => e);
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	numbers = data.numbers.map((e) => e).sort((a: any, b: any) => a.id - b.id);
+	filteredNum = signal(this.numbers);
+	filterNum(
+		input: HTMLInputElement,
+		checkbox: HTMLInputElement,
+		select: HTMLSelectElement,
+	) {
+		if (checkbox.checked) {
+			this.filteredNum.set(this.filteredNum().filter((e) => e.owner !== ""));
+		} else {
+			this.filteredNum.set(this.numbers);
+		}
+		if (select.value === "asc") {
+			this.filteredNum.set(
+				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+				this.filteredNum().sort((a: any, b: any) => a.id - b.id),
+			);
+		} else if (select.value === "desc") {
+			this.filteredNum.set(
+				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+				this.filteredNum().sort((a: any, b: any) => b.id - a.id),
+			);
+		}
+
+		this.filteredNum.set(
+			this.filteredNum().filter((e) => e.id.includes(input.value)),
+		);
+	}
 }
