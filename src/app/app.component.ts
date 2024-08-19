25a18f161673bd190d460a1aac448b988caa245f
Buskik <rdrade121@gmail.com>
2024-08-19 18:29:12 -0300

    Add random owned numbers & sizeable number array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import data from "../data.json";
 
 @Component({
 	selector: "app-root",
@@ -10,19 +9,37 @@ import data from "../data.json";
 	styleUrl: "./app.component.less",
 })
 export class AppComponent {
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	numbers = data.numbers.map((e) => e).sort((a: any, b: any) => a.id - b.id);
+	getData() {
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		const data: Array<any> = [];
+		for (let i = 1; i < 501; i++) {
+			const num = {
+				id: i.toString(),
+				owner: "",
+			};
+			if (i % Math.floor(Math.random() * 10) === 0) {
+				num.owner = "Buskik";
+			}
+			data.push(num);
+		}
+		return data;
+	}
+	data = this.getData();
+	numbers = this.data.map((e) => e).sort((a: any, b: any) => a.id - b.id);
 	filteredNum = signal(this.numbers);
 	filterNum(
 		input: HTMLInputElement,
 		checkbox: HTMLInputElement,
 		select: HTMLSelectElement,
 	) {
+		//Filter to only available numbers if checkbox checked
 		if (checkbox.checked) {
-			this.filteredNum.set(this.filteredNum().filter((e) => e.owner !== ""));
+			this.filteredNum.set(this.filteredNum().filter((e) => !e.owner));
 		} else {
 			this.filteredNum.set(this.numbers);
 		}
+
+		// Sort ascending or descending by selectionn
 		if (select.value === "asc") {
 			this.filteredNum.set(
 				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -35,6 +52,7 @@ export class AppComponent {
 			);
 		}
 
+		//Filter numbers by search
 		this.filteredNum.set(
 			this.filteredNum().filter((e) => e.id.includes(input.value)),
 		);
